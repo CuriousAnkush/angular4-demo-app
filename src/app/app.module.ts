25fe17b87b01af6ac7ec42c6780862cfc8f04fdb
Ankush Maheshwari <ankush_maheshwari@external.mckinsey.com>
2017-12-15 16:14:16 +0530

    Ankush: Angular components walkthrough

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,6 @@
+import {Hero} from '../hero';
+
+export const HEROES: Hero[] = [
+  new Hero(11, 'Mr. Nice', 'Ankush'),
+];
+
